ea530ecc1a80b7f8e47a65ea7012dd1295d6598f
Steven Crouse <sdcrouse91@gmail.com>
2018-05-16 22:25:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function getFirstSelector(selector){
 function nestedTarget() {
   return document.querySelector('#nested').querySelector('.target');
   // Or, I could simplify it to document.querySelector('#nested .target');
-  /* Note: if I said document.querySelector('#nested.target'), then that looks for an element with a 'nested' id AND a 'target' className. We need an element with a .target className that's DESCENDED from an element with a #nested id! */
+  /* Note: if I said document.querySelector('#nested.target'), then that looks for an element with a 'nested' id AND a 'target' className. I need an element with a .target className that's DESCENDED from an element with a #nested id! */
 }
 
 function increaseRankBy(n){
@@ -19,5 +19,16 @@ function increaseRankBy(n){
      elements[i].children[p].innerHTML = parseInt(elements[i].children[p].innerHTML) + n; /* Get the innerHTML of each child of the ul.ranked-list element. Then, parse it for its number and increment it by n. */
     }
   } 
-} /* Admittedly, this looks confusing. Maybe make separate for loops next time and/or make a variable for each ranked-list. */
+  return;
+} /* Admittedly, this looks confusing. Next time, I'll do what they did (see the solution for more details.  */
 
+function deepestChild(){
+  var deepestNode = document.querySelector('#grand-node');
+  /* Note: deepestNode as defined here is an element, not an array. So, deepestNode[0] is invalid. However, deepestNode.children IS an array, so deepestNode.children[0] IS valid! */
+  
+  while(deepestNode.children[0]){
+    deepestNode = deepestNode.children[0];
+  } /* I know that #grand-node has one child, and that each of #grand-node's  descendants has one child, except for the last one. So, keep looping through each descendant until I find the last one. */
+  
+  return deepestNode;
+} /* Hmm...I wonder how I can (in the future, for extra credit :) ) make this find the deepest child when there are sibling nodes? I think I'd need to make two counter variables: one for the highest number of descendants in a node, and one for the number of descendants in the current node. I'd also need a variable to set to the (current) deepest node. */
